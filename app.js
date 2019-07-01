e039560f57fb64fbda56b12c4987e90fe6734caa
masterShip123 <acs57011210007@gmail.com>
2019-07-01 19:12:14 +0700

    First Commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+var fs = require('fs')
+var imageDir = 'D:/back/Uploade/cc/HHhQtudyrnsGbJsqsf5PJxT0.jpg'
+var jpeg = require('jpeg-js');
+var png = require("upng-js");
+const app = express()
+const port = 5000
+const bodyParser = require("body-parser");
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart({ uploadDir: '//192.168.228.115/DirTest' });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/upload", function(request, response, next){
+    next();
+});
+  
+app.get('/api/upload/:id', (req, res)=>{
+    // res.json({'message':'hello'});
+    var sp = req.params['id'].split(".");
+    console.log(sp);
+    if(sp[1] == 'jpg' || sp[1] == 'jpeg'){
+        var jpegData = fs.readFileSync("D:/back/Uploade/cc/"+req.params['id']);
+        // var jpegData = fs.readFileSync(imageDir);
+        var raw = jpeg.decode(jpegData);
+        res.setHeader("Content-Type", "image/jpeg");
+        res.send(jpegData);
+        console.log(req.params['id']);
+    }else{
+        var pangData = fs.readFileSync("D:/back/Uploade/cc/"+req.params['id']);
+        // var jpegData = fs.readFileSync(imageDir);
+        var raw = png.decode(pangData);
+        res.setHeader("Content-Type", "image/png");
+        res.send(pangData);
+        console.log(req.params['id']);
+    }
+   
+    // res.setHeader("Content-Type", "image/png");
+   
+//   fs.readFile(imageDir,function(err,data){
+//       if(err){throw err;}
+//       console.log("Reading file async");
+//     //   console.log(data);
+//       res.send(data);
+//   });
+
+});
+
+app.post('/api/upload', multipartMiddleware, (req, res)=>{
+    res.json({'message1':req.files});
+});
+
+app.use(function(err, req, res, next) {
+    res.json({'error':err.message})
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
